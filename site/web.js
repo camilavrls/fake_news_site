4902a3a2023a7d3791be413edc3047f95b685487
Camila Sousa <camilamorgada7@gmail.com>
2023-09-23 22:48:28 -0300

    retirando comentário e organizando carrossel

--- site/web.js
+++ site/web.js
@@ -16,7 +16,7 @@ function nextSlide() {
 }
 
 function prevSlide() {
-    currentIndex = (currentIndex +1 + slides.length) % slides.length;
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
     showSlide(currentIndex);
 }
 
